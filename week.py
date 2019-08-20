cc8a5b7f0714709dd3b3b66d7343a3b6aa6527b3
Elmokh <bfelmokh@users.noreply.github.com>
2019-08-20 20:37:14 +0200

    v0.1
    # 19/08/2019
    + Absences Check enhanced : Checking by role added

--- week.py
+++ week.py
@@ -14,5 +14,20 @@ class week:
         # return res
     def checkABS(self):
         for i in range(len(self.initw)):
-            if self.initw[i].isABS():
-                print(self.initw[i]," est absent, on doit le permuter")
+            if (self.initw[i].isABS()) and (i not in (1,2,3)):
+                #print(self.initw[i]," est absent, on doit le permuter")
+                if i == 0 :
+                    print("Soir",self.initw[i].isDPM())
+                elif i == 4 :
+                    print("Early",self.initw[i].isDPM())
+                elif i == 5 :
+                    print("Early",self.initw[i].isDPM())
+                elif i == 6 :
+                    print("Late",self.initw[i].isDPM())
+                elif i == 7 :
+                    print("Late",self.initw[i].isDPM())
+                elif i == 8 :
+                    print("Soir",self.initw[i].isDPM())
+                elif i == 9 :
+                    print("Weekend")                    
+                
